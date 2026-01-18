5aec2aa5183f976826cc64d4dfcff2178b63bdfc
Jared Martin <jaredtmartin@gmail.com>
2026-01-18 13:30:58 -0600

    added svg line

--- element.go
+++ element.go
@@ -560,7 +560,8 @@ func (e *DefaultElement) HXDelete(url string) Element {
 
 // Sets the hx-confirm attribute of the element.
 func (e *DefaultElement) HXConfirm(prompt string) Element {
-	e.add_attribute("hx-confirm", prompt)
+	e.add_attribute("hx-confirm", "true")
+	e.add_attribute("hx-confirm-question", prompt)
 	return e
 }
 
